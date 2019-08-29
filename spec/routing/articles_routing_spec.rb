aefba7ec35b6df894a95cc68c89dfdb9ede3bbbc
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-08-29 11:12:12 +0200

    Create article's show route

--- spec/routing/articles_routing_spec.rb
+++ spec/routing/articles_routing_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe ArticlesController, type: :routing do
   it "routes /articles to articles index" do
     expect(get: "/articles").to route_to("articles#index")
   end
+
+  it "routes /articles/:id to articles show" do
+    expect(get: "/articles/1").to route_to("articles#show", id: "1")
+  end
 end
